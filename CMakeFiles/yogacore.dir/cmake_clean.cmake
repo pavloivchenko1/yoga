75d2fefb39a3bd633eeef3e08042a37d0717a1a9
Hernand Azevedo <hernand.azevedo@zup.com.br>
2021-07-06 18:42:24 -0300

    :sparkles: Adds config to generate windows dll
    Signed-off-by: Hernand Azevedo <hernand.azevedo@zup.com.br>

---
+++ CMakeFiles/yogacore.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yogacore.dir/yoga/event/event.cpp.obj"
+  "CMakeFiles/yogacore.dir/yoga/internal/experiments.cpp.obj"
+  "CMakeFiles/yogacore.dir/yoga/log.cpp.obj"
+  "CMakeFiles/yogacore.dir/yoga/Utils.cpp.obj"
+  "CMakeFiles/yogacore.dir/yoga/YGConfig.cpp.obj"
+  "CMakeFiles/yogacore.dir/yoga/YGEnums.cpp.obj"
+  "CMakeFiles/yogacore.dir/yoga/YGLayout.cpp.obj"
+  "CMakeFiles/yogacore.dir/yoga/YGNode.cpp.obj"
+  "CMakeFiles/yogacore.dir/yoga/YGNodePrint.cpp.obj"
+  "CMakeFiles/yogacore.dir/yoga/YGStyle.cpp.obj"
+  "CMakeFiles/yogacore.dir/yoga/YGValue.cpp.obj"
+  "CMakeFiles/yogacore.dir/yoga/Yoga.cpp.obj"
+  "libyogacore.pdb"
+  "libyogacore.dll"
+  "libyogacore.dll.a"
+  "libyogacore.dll.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yogacore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
